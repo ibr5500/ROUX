74c8e95e191554de0d4cbe7a49a92969bfa2beda
unknown <ibr.s.ahmat@gmail.com>
2022-11-15 15:53:49 +0300

    Setup the project

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, './static')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './static/index.html'));
+});
+
+app.get('/speakers', (req, res) => {
+  res.sendFile(path.join(__dirname, './static/speakers.html'));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './static/index.html'));
+});
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`The server listing on Port ${port}`);
+});
