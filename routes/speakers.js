e51d5c4bf07fbe321afebd0e427dc49c18e4f725
unknown <ibr.s.ahmat@gmail.com>
2022-11-16 19:31:02 +0300

    make the web more dynamically

--- routes/speakers.js
+++ routes/speakers.js
@@ -4,12 +4,17 @@ const router = express.Router();
 
 module.exports = (params) => {
   const { speakersService } = params;
-  router.get('/', async (req, res) => {
-    const speakers = await speakersService.getList();
-    return res.json(speakers);
-  });
 
-  router.get('/:shotname', (req, res) => res.send(`Details page of ${req.params.shotname}`));
+  router.get('/', async (req, res) => {
+    const artwork = await speakersService.getAllArtwork();
+    const speakers = await speakersService.getList();
+    res.render('layout', { pageTitle: 'Speakers', template: 'speakers', speakers, artwork });
+  });
+  router.get('/:shotname', async (req, res) => {
+    const speaker = await speakersService.getSpeaker(req.params.shotname);
+    const artwork = await speakersService.getArtworkForSpeaker(req.params.shotname);
+    res.render('layout', { pageTitle: 'Speakers', template: 'speaker-details', speaker, artwork });
+  });
 
   return router;
 };
